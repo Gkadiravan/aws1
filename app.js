35632b315a2ab2b737c4353fbd55647eb3558e41
Dr G Kadiravan <35128052+Gkadiravan@users.noreply.github.com>
2024-03-26 16:29:57 +0530

    app1.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const {MongoClient} = require('mongodb');
-const fileupload = require('express-fileupload');
 
 const app = express();
 app.use(express.json());
